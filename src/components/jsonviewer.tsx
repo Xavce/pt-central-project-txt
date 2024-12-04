1cba4ffd5b9e630699bca02fdea83b3058c3e1c0
Xavce <celio.xaviero123@gmail.com>
2024-12-04 21:45:03 +0700

    creating json viewer component

--- src/components/jsonviewer.tsx
+++ src/components/jsonviewer.tsx
@@ -1,10 +1,11 @@
 import JsonView from '@uiw/react-json-view';
 import { githubDarkTheme } from '@uiw/react-json-view/githubDark';
+import React from "react"
 
 interface JsonDataProps {
     jsonData: object;
 }
-export const JsonViewerComponent:React.FC<JsonDataProps> = ({jsonData}) => {
+export const JsonViewerComponent:React.FC<JsonDataProps> = React.memo(({jsonData}) => {
 
     return(
         <JsonView
@@ -12,4 +13,4 @@ export const JsonViewerComponent:React.FC<JsonDataProps> = ({jsonData}) => {
             style={githubDarkTheme}
             value={jsonData} />
     )
-}
+})
