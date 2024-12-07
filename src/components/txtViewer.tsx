2358c059eda073f013519311158f982879488a7a
Xavce <celio.xaviero123@gmail.com>
2024-12-07 10:59:06 +0700

    adding txt viewer

---
+++ src/components/txtViewer.tsx
@@ -0,0 +1,10 @@
+const TxtViewer = ({ textString }) => {
+
+    return (
+        <div style={{whiteSpace: "pre-wrap"}}>
+            {textString}
+        </div>
+    );
+};
+
+export default TxtViewer;
